f8aace05e2cba7b3dccfcadf503284415a4f04b3
archivers <archivers@gmail.com>
2022-08-08 12:35:39 -0500

    can change grid size and select pen/eraser

--- js/script.js
+++ js/script.js
@@ -2,19 +2,31 @@ let gridBlockCount = 13;
 
 let gridWidth = document.getElementById("grid").clientWidth;
 
-for (let i=0;i<13;i++) {
-  document.getElementById("grid").appendChild(document.createElement("data-row"));
-}
+
 
 const dataGrid = document.getElementsByTagName("data-row");
-for (let grid of dataGrid) {
-  for(let i=0;i<13;i++) {
-    grid.appendChild(document.createElement("data-cell"));
-  }  
+const grid = document.getElementById("grid");
+
+function drawGrid(range) {
+  grid.textContent = '';
+  
+  for (let i=0;i<range*16;i++) {
+    document.getElementById("grid").appendChild(document.createElement("data-row"));
+  }
+
+  for (let grid of dataGrid) {
+    for(let i=0;i<range*16;i++) {
+      grid.appendChild(document.createElement("data-cell"));
+    }  
+  }
+  colorGrid();
 }
 
 let isMouseDown = false;
+let isEraserActive = false;
+let isPenActive = true;
 colorGrid();
+drawGrid(1);
 
 function colorGrid() {
   const elements = document.querySelectorAll("data-cell");
@@ -22,8 +34,13 @@ function colorGrid() {
     element.addEventListener('mouseenter', (e)=> {
       if(isMouseDown){
         const node = e.target;
-        node.style.backgroundColor = "red";
-      }
+        
+        if(isEraserActive){
+          node.style.backgroundColor = "black";
+        } else if(isPenActive) {
+          node.style.backgroundColor = "red";
+        }
+     }
     });
   });
 }
@@ -35,14 +52,35 @@ document.body.onmouseup = ()=> {isMouseDown = false; console.log(isMouseDown);}
 const clearButt = document.getElementById("clearButt");
 clearButt.addEventListener("click", clearGrid);
 const eraseButt = document.getElementById("eraseButt");
-eraseButt.addEventListener("click", clearGrid);
+eraseButt.addEventListener("click", eraserGrid);
+const colorButt = document.getElementById("colorButt");
+colorButt.addEventListener("click", penGrid);
+const rangeButt = document.getElementById("rangeButt");
+rangeButt.addEventListener("change", updateGridSize);
+
+function updateGridSize(e) {
+  drawGrid(rangeButt.value);
+}
 
 function clearGrid(e) {
-  setActiveButton(e);
   const elements = document.querySelectorAll("data-cell");
   elements.forEach(element => {
     element.style.backgroundColor = "black";
   });
+}
+
+function eraserGrid(e) {
+  setActiveButton(e);
+  isEraserActive = true;
+  isPenActive = false;
+  console.log("isEraseActive = " +isEraserActive);
+}
+
+function penGrid(e) {
+  setActiveButton(e);
+  isPenActive = true;
+  isEraserActive = false;
+  console.log("isPenActive = " + isPenActive);
 }
 
 function setActiveButton(e) {
